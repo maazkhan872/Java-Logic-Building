375381a145bf960240d9a65f291c8ec7633e5903
Muhammad Maaz Khan <maazkhank872@gmail.com>
2026-02-24 23:18:43 +0500

    Add another concept Implicit and Explicit Problems

---
+++ src/Java_Coding_Working/Impli_and_expli.java
@@ -0,0 +1,15 @@
+package Java_Coding_Working;
+
+public class Impli_and_expli {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+
+        double b = a;
+
+        System.out.println(b);
+
+    }
+
+}
