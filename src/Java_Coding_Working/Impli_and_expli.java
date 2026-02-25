5f4eaca22e2c3711a77d8863a4a9e7ceddb27d81
Muhammad Maaz Khan <maazkhank872@gmail.com>
2026-02-25 22:56:15 +0500

    Update

--- src/Java_Coding_Working/Impli_and_expli.java
+++ src/Java_Coding_Working/Impli_and_expli.java
@@ -1,6 +1,6 @@
 package Java_Coding_Working;
 
-public class Impli_and_expli {
+/*public class Impli_and_expli {
 
     public static void main(String[] args) {
 
@@ -13,3 +13,107 @@ public class Impli_and_expli {
     }
 
 }
+
+public class Impli_and_expli {
+
+    public static void main(String[] args) {
+
+        char ch = 'A';
+
+        int num = ch;
+
+        System.out.println(num);
+
+    }
+
+}
+
+public class Impli_and_expli {
+
+    public static void main(String[] args) {
+
+        int x = 100;
+
+        long y = x;
+
+        float z = y;
+
+        System.out.println(z);
+
+    }
+}
+
+public class Impli_and_expli {
+
+    public static void main(String[] args) {
+
+        double a = 10.5;
+
+        int b = (int) a;
+
+        System.out.println(b);
+
+    }
+}
+
+public class Impli_and_expli {
+
+    public static void main(String[] args) {
+
+        int num = 130;
+
+        byte b = (byte) num;
+
+        System.out.println(b);
+
+    }
+}
+
+public class Impli_and_expli {
+
+    public static void main(String[] args) {
+
+        float f = 9.8f;
+
+        int x = (int) f;
+
+        System.out.println(x);
+
+    }
+
+}
+
+public class Impli_and_expli {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+
+        double b = 6.2;
+
+        int result = a + (int) b;
+
+        System.out.println(result);
+
+    }
+}
+
+public class Impli_and_expli {
+
+    public static void main(String[] args) {
+
+        int a = 20;
+
+        double b = a;
+
+        double c = 15.7;
+
+        int d = (int) c;
+
+        System.out.println(b);
+
+        System.out.println(d);
+
+    }
+
+}*/
