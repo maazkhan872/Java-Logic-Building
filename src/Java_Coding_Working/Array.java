e9e21b18d9e2bb1705147a62b1673b197011949a
Muhammad Maaz Khan <maazkhank872@gmail.com>
2026-02-25 22:54:46 +0500

    Updating array files after sorting problems

--- src/Java_Coding_Working/Array.java
+++ src/Java_Coding_Working/Array.java
@@ -343,5 +343,73 @@ public class Array {
         System.out.println("Even count is: " + count);
 
     }
-} 
+}
+
+// sort elements
+public class Array {
+
+    public static void main(String[] args) {
+
+        int arr[] = {5,3,8,1};
+
+        for(int i = 0; i < arr.length; i++) {
+
+            for(int j = i + 1; j < arr.length; j++) {
+
+                if(arr[i] > arr[j]) {
+
+                    int temp = arr[i];
+
+                    arr[i] = arr[j];
+
+                    arr[j] = temp;
+
+                }
+
+            }
+
+        }
+
+        for(int num : arr)
+
+            System.out.print(num + " ");
+
+    }
+}
+
+
+// Find Second Largest Element
+public class Array {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10,20,5,8,30};
+
+        int first = arr[0];
+
+        int second = arr[0];
+
+        for(int num : arr) {
+
+            if(num > first) {
+
+                second = first;
+
+                first = num;
+
+            }
+
+            else if(num > second && num != first) {
+
+                second = num;
+
+            }
+
+        }
+
+        System.out.println("Second largest is: " + second);
+
+    }
+
+}
 
