ca46a3026e6c93ddb6ab2e9db230a30aa292f8fe
Muhammad Maaz Khan <maazkhank872@gmail.com>
2026-03-16 22:11:37 +0500

    Updated if-else logic

--- src/Java_Coding_Working/ifelse_logic.java
+++ src/Java_Coding_Working/ifelse_logic.java
@@ -399,6 +399,25 @@ public class ifelse_logic {
 }
 
 
+public class ifelse_logic {
+    public static void main(String[] args) {
 
+        int marks = 75;
+
+        if(marks >= 90) {
+            System.out.println("Grade A");
+        }
+        else if(marks >= 75) {
+            System.out.println("Grade B");
+        }
+        else if(marks >= 50) {
+            System.out.println("Grade C");
+        }
+        else {
+            System.out.println("Fail");
+        }
+
+    }
+}
 
 
