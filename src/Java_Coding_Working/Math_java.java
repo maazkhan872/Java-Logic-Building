e9fe7d7154d4831b9ea45776ce1b5ba00215449e
Muhammad Maaz Khan <maazkhank872@gmail.com>
2026-04-05 20:05:12 +0500

    Updated

--- src/Java_Coding_Working/Math_java.java
+++ src/Java_Coding_Working/Math_java.java
@@ -91,3 +91,11 @@ public class  Math_java {
         System.out.println(result);
     }
 }
+
+// floor vs ceil
+public class Math_java {
+    public static void main(String[] args) {
+        System.out.println(Math.floor(5.9));
+        System.out.println(Math.ceil(5.1));
+    }
+}
