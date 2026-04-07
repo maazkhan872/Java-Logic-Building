15d25e0bd4f158fa2aaca8cdb90f1fa6c401b7ca
Muhammad Maaz Khan <maazkhank872@gmail.com>
2026-04-07 23:17:51 +0500

    Updated:

--- src/Java_Coding_Working/Loops.java
+++ src/Java_Coding_Working/Loops.java
@@ -1,6 +1,6 @@
 //package Java_Coding_Working;
 
-/*public class Loops {
+public class Loops {
 
 	public static void main(String args[]) {
 		
@@ -182,4 +182,20 @@ public class Loops{
 			
 			
 }
-}*/
+}
+
+
+public class Loops {
+    public static void main(String[] args) {
+        int num = 1234;
+        int reversed = 0;
+
+        while (num != 0) {
+            int digit = num % 10;     // get last digit
+            reversed = reversed * 10 + digit; // build reversed number
+            num = num / 10;          // remove last digit
+        }
+
+        System.out.println("Reversed: " + reversed);
+    }
+}
