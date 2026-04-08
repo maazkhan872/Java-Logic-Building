d2f4635cc648bc20f199c8ff0772d1e6a2383b2c
Muhammad Maaz Khan <maazkhank872@gmail.com>
2026-04-08 23:04:47 +0500

    Added

--- src/Java_Coding_Working/Methods.java
+++ src/Java_Coding_Working/Methods.java
@@ -1,6 +1,6 @@
 package Java_Coding_Working;
 
-/*
+
 // creating a method
 public class Methods {
 	
@@ -256,4 +256,22 @@ public class Methods {
         c1.display();
         c2.display();
     }
-} */
+} 
+
+public class Methods {
+
+    public static int calculateSum(int n) {
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i; // sum = sum + i
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int result = calculateSum(5);
+        System.out.println("Sum: " + result);
+    }
+}
