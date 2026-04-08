d732fae31da9049069347263c21f1da15547c9eb
Muhammad Maaz Khan <maazkhank872@gmail.com>
2026-04-08 22:57:36 +0500

    Added: another logic problem

--- src/Java_Coding_Working/Loops.java
+++ src/Java_Coding_Working/Loops.java
@@ -228,3 +228,18 @@ public class Loops {
         System.out.println("Factorial: " + factorial);
     }
 }
+
+// Count Digits in a Number
+public class Loops {
+    public static void main(String[] args) {
+        int num = 12345;
+        int count = 0;
+
+        while (num != 0) {
+            num = num / 10; // last digit remove
+            count++;        // count increase
+        }
+
+        System.out.println("Digits: " + count);
+    }
+}
