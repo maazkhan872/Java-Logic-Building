c743d63300a10b795f917d13214f84375007799d
Muhammad Maaz Khan <maazkhank872@gmail.com>
2026-04-14 21:42:07 +0500

    Added: Encapsulation logic problem

--- src/Java_Coding_Working/Encapsulation_oop.java
+++ src/Java_Coding_Working/Encapsulation_oop.java
@@ -16,4 +16,55 @@ We achieve encapsulation using:
 
 public class Encapsulation_oop {
 
+    // hidden data (Encapsulation)
+    private double balance;
+
+    // constructor
+    public Encapsulation_oop(double initialBalance) {
+        if (initialBalance >= 0) {
+            this.balance = initialBalance;
+        } else {
+            this.balance = 0;
+        }
+    }
+
+    // ✅ deposit method (controlled access)
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        } else {
+            System.out.println("Invalid deposit amount!");
+        }
+    }
+
+    // withdraw method (controlled access)
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            System.out.println("Invalid withdrawal amount!");
+        } else if (amount > balance) {
+            System.out.println("Insufficient balance!");
+        } else {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        }
+    }
+
+    // read-only access
+    public double getBalance() {
+        return balance;
+    }
+    
+    public static void main(String[] args) {
+
+    	Encapsulation_oop account = new Encapsulation_oop(1000);
+
+        account.deposit(500);
+        account.withdraw(300);
+
+        System.out.println("Current Balance: " + account.getBalance());
+
+        // NOT allowed (because of encapsulation)
+        // account.balance = -9999;
+    }
 }
