3bd0c6c8291c382b9f4aa18d29b01642ab0eb2fa
Muhammad Maaz Khan <maazkhank872@gmail.com>
2026-04-14 21:39:32 +0500

    Add structure of OOP Pillar

---
+++ src/Java_Coding_Working/Encapsulation_oop.java
@@ -0,0 +1,19 @@
+package Java_Coding_Working;
+
+/*
+Note:
+First Pillar of OOP: Encapsulation
+"Wrapping data (variables) and methods (functions) into a single unit (class) and restricting direct access to some of the object’s components"
+"Hide internal details and control access through methods”
+
+How Encapsulation Works in Code:
+We achieve encapsulation using:
+
+1. private variables (data hiding)
+2. public getter/setter methods (controlled access)
+*/
+
+
+public class Encapsulation_oop {
+
+}
