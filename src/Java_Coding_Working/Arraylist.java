6b3c23700e91e7c2fc2b5d354d4a2678533c3681
Muhammad Maaz Khan <maazkhank872@gmail.com>
2026-04-29 23:21:50 +0500

    Added Java program to find duplicate elements in ArrayList using HashSet

--- src/Java_Coding_Working/Arraylist.java
+++ src/Java_Coding_Working/Arraylist.java
@@ -108,3 +108,32 @@ public class Arraylist {
         System.out.println("Updated List: " + names);
     }
 }
+
+// Find Duplicate Elements
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Arraylist {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(10);
+        numbers.add(40);
+        numbers.add(20);
+
+        HashSet<Integer> seen = new HashSet<>();
+        HashSet<Integer> duplicates = new HashSet<>();
+
+        for (int num : numbers) {
+            if (!seen.add(num)) {
+                duplicates.add(num);
+            }
+        }
+
+        System.out.println("Duplicate elements: " + duplicates);
+    }
+}
