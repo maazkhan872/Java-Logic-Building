f3ffb1c9848e0b7f2ce8f0f73ea17d3808ecb14e
Muhammad Maaz Khan <maazkhank872@gmail.com>
2026-05-04 23:03:46 +0500

    Add maximum element logic problem

--- src/Java_Coding_Working/Arraylist.java
+++ src/Java_Coding_Working/Arraylist.java
@@ -219,3 +219,29 @@ public class Arraylist{
         }
     }
 }
+
+
+// Find Maximum Element
+import java.util.ArrayList;
+
+public class Arraylist {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(5);
+        list.add(15);
+        list.add(25);
+        list.add(10);
+
+        int max = list.get(0);
+
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) > max) {
+                max = list.get(i);
+            }
+        }
+
+        System.out.println("Max value: " + max);
+    }
+}
