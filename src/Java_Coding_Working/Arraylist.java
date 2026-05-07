2e8e10156a835b4a409df1925aabe36d5dae9171
Muhammad Maaz Khan <maazkhank872@gmail.com>
2026-05-07 22:38:36 +0500

    Updated size logic problem

--- src/Java_Coding_Working/Arraylist.java
+++ src/Java_Coding_Working/Arraylist.java
@@ -46,6 +46,8 @@ public class Arraylist {
         numbers.add(10);
         numbers.add(20);
         numbers.add(30);
+        numbers.add(40);
+        numbers.add(50);
 
         System.out.println(numbers.size());
     }
