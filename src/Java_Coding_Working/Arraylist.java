ec6608ddfd1c089c5344445c4f9b58fe3e5de8f1
Muhammad Maaz Khan <maazkhank872@gmail.com>
2026-05-20 23:11:04 +0500

    added Find Duplicate Elements logic problem

--- src/Java_Coding_Working/Arraylist.java
+++ src/Java_Coding_Working/Arraylist.java
@@ -104,6 +104,36 @@ public class Arraylist {
     }
 }
 
+// Find Duplicate Elements
+import java.util.ArrayList;
+
+public class Arraylist {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(20);
+        numbers.add(40);
+        numbers.add(10);
+
+        for(int i = 0; i < numbers.size(); i++) {
+
+            for(int j = i + 1; j < numbers.size(); j++) {
+
+                if(numbers.get(i).equals(numbers.get(j))) {
+
+                    System.out.println("Duplicate Element: " + numbers.get(i));
+                }
+            }
+        }
+    }
+}
+
+
+
 import java.util.ArrayList;
 
 public class Arraylist {
