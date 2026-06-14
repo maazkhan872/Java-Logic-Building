5511a9c25c3dbfc66a7ddbb7d9d08ed04d3f84f9
Muhammad Maaz Khan <maazkhank872@gmail.com>
2026-06-14 20:21:40 +0500

    added fizz buzz logic problem

--- src/Java_Coding_Working/Loops.java
+++ src/Java_Coding_Working/Loops.java
@@ -308,3 +308,23 @@ public class Loops {
         }
     }
 }
+
+// fizz Buzz Logic
+public class Loops {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
