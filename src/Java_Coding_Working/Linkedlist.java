a1dbd892e8c6a12250a2b7c11401b83189be2067
Muhammad Maaz Khan <maazkhank872@gmail.com>
2026-07-01 22:32:29 +0500

    add another problem

--- src/Java_Coding_Working/Linkedlist.java
+++ src/Java_Coding_Working/Linkedlist.java
@@ -588,3 +588,28 @@ public class Linkedlist {
         }
     }
 }
+
+// Find the Index of an Element
+import java.util.LinkedList;
+
+public class Linkedlist {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> fruits = new LinkedList<>();
+
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Mango");
+        fruits.add("Orange");
+
+        int index = fruits.indexOf("Mango");
+
+        if (index != -1) {
+            System.out.println("Mango found at index: " + index);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
+
