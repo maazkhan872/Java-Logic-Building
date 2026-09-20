05f47a0a65db2e299d9ba034ba5dda95116b6618
Muhammad Maaz Khan <163487911+maazkhan872@users.noreply.github.com>
2026-09-20 23:37:06 +0500

    Add element 40 to the stack

--- src/Java_Coding_Working/Stack.java
+++ src/Java_Coding_Working/Stack.java
@@ -10,6 +10,7 @@ public class Main {
         stack.push(10);
         stack.push(20);
         stack.push(30);
+        stack.push(40);
 
         // Print top element
         System.out.println(stack.peek());
