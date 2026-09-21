37f999e22ab2c95300aa09d87727c71284e94036
Muhammad Maaz Khan <163487911+maazkhan872@users.noreply.github.com>
2026-09-21 21:45:26 +0500

    Add main method to demonstrate Stack usage

--- src/Java_Coding_Working/Stack.java
+++ src/Java_Coding_Working/Stack.java
@@ -85,3 +85,23 @@ public class Main {
         System.out.println("Stack: " + stack);
     }
 }
+
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+
+        System.out.println("Stack: " + stack);
+
+        System.out.println("Size: " + stack.size());
+    }
+}
