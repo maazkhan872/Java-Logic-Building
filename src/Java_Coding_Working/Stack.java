1cc5d426ee74c2324eb38bf1f8223e0f7122e23d
Muhammad Maaz Khan <163487911+maazkhan872@users.noreply.github.com>
2026-09-21 21:46:17 +0500

    Add Main class to demonstrate Stack functionality

--- src/Java_Coding_Working/Stack.java
+++ src/Java_Coding_Working/Stack.java
@@ -105,3 +105,25 @@ public class Main {
         System.out.println("Size: " + stack.size());
     }
 }
+
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+
+        System.out.println("Stack elements:");
+
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            System.out.println(stack.get(i));
+        }
+    }
+}
