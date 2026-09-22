48934c1b37548c258c3bae8b7a31aebfce337c75
Muhammad Maaz Khan <163487911+maazkhan872@users.noreply.github.com>
2026-09-22 21:59:41 +0500

    Add main method to demonstrate Stack functionality

--- src/Java_Coding_Working/Stack.java
+++ src/Java_Coding_Working/Stack.java
@@ -152,3 +152,27 @@ public class Main {
         System.out.println("Stack is empty: " + stack.isEmpty());
     }
 }
+
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+
+        int search = 30;
+
+        if (stack.contains(search)) {
+            System.out.println(search + " is present in Stack");
+        } else {
+            System.out.println(search + " is not present in Stack");
+        }
+    }
+}
